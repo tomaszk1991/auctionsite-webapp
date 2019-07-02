540ff2accf38be70f9f2364a425af2215f32fd2e
Tomasz <kubicek12345@gmail.com>
2019-07-02 17:09:36 +0200

    class address - added variables

--- src/main/java/pl/sda/webappaucitonsite/data/Address.java
+++ src/main/java/pl/sda/webappaucitonsite/data/Address.java
@@ -1,4 +1,23 @@
 package pl.sda.webappaucitonsite.data;
 
+import lombok.Data;
+
+@Data
 public class Address {
+    private String street;
+    private String buildingNumber;
+    private String houseNumber;
+    private String province;
+    private String city;
+    private String ZIPCode;
+
+    public Address(String street, String buildingNumber, String province, String city, String ZIPCode) {
+        this.street = street;
+        this.buildingNumber = buildingNumber;
+        this.province = province;
+        this.city = city;
+        this.ZIPCode = ZIPCode;
+    }
 }
+
+

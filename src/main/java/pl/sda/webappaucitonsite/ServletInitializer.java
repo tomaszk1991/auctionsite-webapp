d83c0f24ba3cd92856557168595f62a8465dc58c
Tomasz <kubicek12345@gmail.com>
2019-07-02 16:50:39 +0200

    springboot start

---
+++ src/main/java/pl/sda/webappaucitonsite/ServletInitializer.java
@@ -0,0 +1,13 @@
+package pl.sda.webappaucitonsite;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebappAucitonsiteApplication.class);
+	}
+
+}
---
+++ src/main/java/pl/sda/webappaucitonsite/WebappAucitonsiteApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.webappaucitonsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappAucitonsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappAucitonsiteApplication.class, args);
+	}
+
+}
